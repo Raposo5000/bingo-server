b1c4ce504f2fd3e465a849c679b80f6d2c2fd028
Felipe Santos <uchihafelip3@gmail.com>
2021-10-19 16:48:23 -0300

    feat: route to get all rooms

--- lib/interfaces/routes/routes.ts
+++ lib/interfaces/routes/routes.ts
@@ -1,10 +1,16 @@
-import { Response, Router } from 'express';
+import { Response, Router } from "express";
+import { Rooms } from "../../domain/Rooms";
 
 const routes = Router();
 
-routes.get('/', (_, res: Response) => {
-  res.send('Hello World!');
+routes.get("/", (_, res: Response) => {
+  res.send("Hello World!");
 });
 
-export { routes }
+routes.get("/rooms", async (_, res: Response) => {
+  const rooms = Rooms.getInstance();
+  const allRooms = rooms.getRooms();
+  res.send(allRooms);
+});
 
+export { routes };
